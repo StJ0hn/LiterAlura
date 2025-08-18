209f535d69c7729ae24925485613bb1e81e83f3a
StJ0hn <jonhmiguel138@gmail.com>
2025-08-18 18:48:37 -0300

    Todas as funcionalidades de consulta inseridas.

--- src/main/java/com/challange/literaAlura/repository/AutorRepository.java
+++ src/main/java/com/challange/literaAlura/repository/AutorRepository.java
@@ -1,4 +1,12 @@
 package com.challange.literaAlura.repository;
 
-public interface AutorRepository {
+import com.challange.literaAlura.model.Autor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface AutorRepository extends JpaRepository<Autor, Long> {
+    Optional<Autor> findByNomeContainingIgnoreCase(String nome);
+    List<Autor> findByAnoDeNascimentoLessThanEqualAndAnoDeFalecimentoGreaterThanEqual(Integer anoNascimento, Integer anoFalecimento);
 }
