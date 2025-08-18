1b8bfe357ca4de625b91c912ce3b440b0aff38aa
StJ0hn <jonhmiguel138@gmail.com>
2025-08-18 13:35:56 -0300

    Configurações iniciais do projeto

---
+++ src/main/java/com/challange/literaAlura/LiteraAluraApplication.java
@@ -0,0 +1,13 @@
+package com.challange.literaAlura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraAluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraAluraApplication.class, args);
+	}
+
+}
